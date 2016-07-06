53262a8843a66b592594e2ab5468b094ad5b94c7
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-07-06 14:51:18 +0200

    Adding license headers

--- application/code/app/dirPagination.js
+++ application/code/app/dirPagination.js
@@ -1,17 +1,29 @@
 /**
- * dirPagination - AngularJS module for paginating (almost) anything.
- *
- *
- * Credits
- * =======
- *
- * Daniel Tabuenca: https://groups.google.com/d/msg/angular/an9QpzqIYiM/r8v-3W1X5vcJ
- * for the idea on how to dynamically invoke the ng-repeat directive.
- *
- * I borrowed a couple of lines and a few attribute names from the AngularUI Bootstrap project:
- * https://github.com/angular-ui/bootstrap/blob/master/src/pagination/pagination.js
- *
- * Copyright 2014 Michael Bromley <michael@michaelbromley.co.uk>
+ * Copyright (c) 2015 SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
+ * ALL RIGHTS RESERVED.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Neither the name of the SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
+ * nor the names of its contributors may be used to endorse or promote 
+ * products derived from this software without specific prior written 
+ * permission.
+ * 
+ * This work has been performed in the framework of the SONATA project,
+ * funded by the European Commission under Grant number 671517 through 
+ * the Horizon 2020 and 5G-PPP programmes. The authors would like to 
+ * acknowledge the contributions of their colleagues of the SONATA 
+ * partner consortium (www.sonata-nfv.eu).* dirPagination - AngularJS module for paginating (almost) anything.
  */
 
 (function() {
